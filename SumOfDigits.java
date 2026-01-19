0e104325c31e06ffe322db2391b1c7a8e87082d7
Sakshi Babar <sakshibabar17@gmail.com>
2026-01-19 21:05:54 +0530

    Add SumOfDigits program to calculate digit sum

---
+++ SumOfDigits.java
@@ -0,0 +1,22 @@
+//Java program to find sum of digits of number
+
+import java.util.Scanner;
+
+class SumOfDigits{
+  public static void main(String args[]){
+
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int sum = 0;
+
+    while(num != 0){
+      int digit = num % 10;
+      sum = sum + digit;
+      num = num / 10;
+
+    }
+
+    System.out.println("Sum of digits= "+sum);
+    sc.close();
+  }
+}
