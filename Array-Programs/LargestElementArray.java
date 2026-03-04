aac6308285274fc362d9578b3b58fae00a650f09
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-04 21:44:31 +0530

    find largest element from array
    Fixed loop condition to prevent ArrayIndexOutOfBoundsException and corrected the input prompt.

--- Array-Programs/LargestElementArray.java
+++ Array-Programs/LargestElementArray.java
@@ -1,1 +1,32 @@
+//Java program to find largest element from an array
 
+import java.util.Scanner;
+
+class LargestElementArray{
+
+  public static void main(String args[]){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter number of elements: ");
+    int n = sc.nextInt();
+
+    int arr[] = new int[n];
+
+    System.out.println("Enter array elements: ");
+
+    for (int i = 0; i <=n ; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++){
+      if (arr[i] > largest){
+        largest = arr[i];
+      }
+    }
+
+    System.out.println("Largest element: " +largest);
+  }
+}
