8d0930980c59a401dab78a3883b8a5e70851cd27
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-04 21:53:27 +0530

    Add Java program to reverse an array

--- Array-Programs/ReverseArray.java
+++ Array-Programs/ReverseArray.java
@@ -1,1 +1,28 @@
+//Java program to print reverse of an array
 
+import java.util.Scanner;
+
+class ReverseArray{
+
+  public static void main(String args[]){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter number of elements : ");
+    int n = sc.nextInt();
+
+    int arr[] = new int[n];
+
+    System.out.println("Enter array elements: ");
+
+    for (int i = 0; i < n ; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    System.out.println("Reversed array: ");
+
+    for ( int i = n - 1; i >= 0 ; i--){
+      System.out.println (arr[i] + " ");
+    }
+  }
+}
