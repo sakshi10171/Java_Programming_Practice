3c2462d4a375a2ca1d8a914f4521850dac15f354
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-04 21:37:39 +0530

    Correct loop condition for array input
    Fix array index out of bounds error in the loop.

--- Array-Programs/ArraySum.java
+++ Array-Programs/ArraySum.java
@@ -1,1 +1,26 @@
+//Java program to find sum of elements in array
 
+import java.util.Scanner;
+
+class ArraySum{
+  public static void main(String args[]){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter number of elements: ");
+    int n = sc.nextInt();
+
+    int arr[] = new int[n];
+    int sum = 0;
+
+    System.out.println("Enter array elements: ");
+
+    for (int i = 0; i <= n ; i++){
+      arr[i] = sc.nextInt();
+      sum = sum + arr[i];
+    }
+
+    System.out.println("Sum of array elements: " +sum);
+
+  }
+}
