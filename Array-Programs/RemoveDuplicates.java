165413c6abb0b831c99bbdbe9f561c4e600aea3b
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-17 18:54:36 +0530

    Add RemoveDuplicates.java to remove duplicates from array
    This Java program removes duplicate elements from an array by checking each element against previously encountered elements.

---
+++ Array-Programs/RemoveDuplicates.java
@@ -0,0 +1,39 @@
+//Java program to remove duplicate elements from an array
+
+import java.util.Scanner;
+
+class RemoveDuplicates{
+
+  public static void main(String args[]){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter number of elements: ");
+    int n = sc.nextInt();
+
+    int arr[] = new int[n];
+
+    System.out.println("Enter array elements: ");
+
+    for ( int i = 0 ; i < n ; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    System.out.println("Array without duplicates");
+
+    for ( int i = 0 ; i < n ; i++){
+      boolean isDuplicate = false;
+
+      for(int j = 0 ; j < i ; j++){
+        if(arr[i] == arr[j]){
+          isDuplicate = true;
+          break;
+        }
+      }
+
+      if(!isDuplicate){
+        System.out.print(arr[i] +  " ");
+      }
+    }
+  }
+}
