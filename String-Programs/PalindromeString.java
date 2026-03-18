afc334bffb78cbfe64b69dc1155e5606074a6ec0
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-18 17:52:22 +0530

    Add PalindromeString.java to check for palindromes

---
+++ String-Programs/PalindromeString.java
@@ -0,0 +1,26 @@
+//Java program to check string is palindrome or not
+
+import java.util.Scanner;
+
+class PalindromeString{
+
+  public static void main(String args[]){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter a string: ");
+    String str = sc.nextLine();
+
+    String rev = "";
+
+    for ( int i = str.length() - 1 ;  i >= 0 ; i--){
+      rev = rev + str.charAt(i);
+    }
+
+    if(str.equals(rev)){
+      System.out.println("Palindrome string");
+    }else{
+      System.out.println("Not a palindrome string");
+    }
+  }
+}
