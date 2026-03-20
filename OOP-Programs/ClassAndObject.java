77b6babbc884d4448b4352f4ea0aedf3e92c63df
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-20 21:09:10 +0530

    Implement basic class and object example in Java
    Added a simple Java program demonstrating class and object concepts in OOP.

--- OOP-Programs/ClassAndObject.java
+++ OOP-Programs/ClassAndObject.java
@@ -1,1 +1,25 @@
+//Java program to understand class and object in oops
 
+import java.util.Scanner;
+
+class Student{
+  int  id;
+  String name;
+
+  void display() {
+    System.out.println("ID: " + id);
+    System.out.println("Name: " + name);
+  }
+}
+
+public class ClassAndObject{
+
+  public static void main(String args[]){
+
+    Student s1 = new Student();
+    s1.id = 1;
+    s1.name = "Sakshi";
+
+    s1.display();
+  }
+}
