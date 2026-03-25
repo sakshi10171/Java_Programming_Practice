c8e2de47b8ec8f170a9ee03a8db3d57dbd416d14
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-25 20:26:57 +0530

    Create Armstrong.java

---
+++ Basic-Programs/Armstrong.java
@@ -0,0 +1,25 @@
+//java program to check number is armstrong or not
+
+import java.util.Scanner;
+
+class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int original = num, sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        if (sum == original)
+            System.out.println("Armstrong");
+        else
+            System.out.println("Not Armstrong");
+
+        sc.close();
+    }
+}
