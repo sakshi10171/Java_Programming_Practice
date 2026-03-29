4fd9dd2c44f9dc5314909d27644fd8373e5747f8
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-29 17:54:16 +0530

    Add files via upload

---
+++ if-else/EvenOdd.java
@@ -0,0 +1,21 @@
+//java program to check number is even or odd
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even number");
+        } else {
+            System.out.println(num + " is Odd number");
+        }
+        sc.close();
+    }
+}
