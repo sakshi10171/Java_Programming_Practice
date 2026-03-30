c6b7c02502cd735842b185542566354451be168f
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-30 22:54:16 +0530

    Add AbsoluteValue.java to compute absolute values

---
+++ if-else/AbsoluteValue.java
@@ -0,0 +1,22 @@
+//java program to convert absolute value into positive value
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num < 0) {
+            num = -num;
+        }
+
+        System.out.println("Absolute value: " + num);
+
+        sc.close();
+    }
+}
