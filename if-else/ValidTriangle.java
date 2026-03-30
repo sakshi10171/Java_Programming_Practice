f5bfdc6778f78c0f531f9497e771ab9fecf7813c
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-30 22:48:17 +0530

    Add ValidTriangle.java to check triangle validity

---
+++ if-else/ValidTriangle.java
@@ -0,0 +1,24 @@
+//java program to check triangle is valid or not by checking sum of two sides is greater than third side
+
+import java.util.Scanner;
+
+public class TriangleValid {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter three sides: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("Valid Triangle");
+        } else {
+            System.out.println("Invalid Triangle");
+        }
+
+        sc.close();
+    }
+}
