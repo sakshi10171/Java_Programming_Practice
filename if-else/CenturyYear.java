84389ccab15f4415ca27663c4d8e65ebed38a0b2
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-31 23:25:48 +0530

    Add CenturyYear program to check century years

---
+++ if-else/CenturyYear.java
@@ -0,0 +1,22 @@
+//java program to check year is century year or not
+
+import java.util.Scanner;
+
+public class CenturyYear {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if (year % 100 == 0) {
+            System.out.println("Century Year");
+        } else {
+            System.out.println("Not a Century Year");
+        }
+
+        sc.close();
+    }
+}
