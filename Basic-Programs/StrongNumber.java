e68d168fed5fd7f3b317b53addad836415256a21
Sakshi Babar <sakshibabar17@gmail.com>
2026-04-01 23:28:37 +0530

    Add StrongNumber program to check strong numbers
    This program checks if a given number is a strong number, which means the sum of the factorials of its digits equals the number itself.

---
+++ Basic-Programs/StrongNumber.java
@@ -0,0 +1,35 @@
+//java program to check number is strong number or not
+
+import java.util.Scanner;
+
+class StrongNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            int fact = 1;
+
+            for (int i = 1; i <= digit; i++) {
+                fact *= i;
+            }
+
+            sum += fact;
+            num /= 10;
+        }
+
+        if (sum == original)
+            System.out.println("Strong Number");
+        else
+            System.out.println("Not Strong Number");
+
+        sc.close();
+    }
+}
