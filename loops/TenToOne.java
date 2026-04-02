063480601b27b8cec3c42d689dc7326e73985f94
Sakshi Babar <sakshibabar17@gmail.com>
2026-04-02 21:19:32 +0530

    Create TenToOne.java to print numbers in reverse
    Add a Java program to print numbers from 10 to 1.

---
+++ loops/TenToOne.java
@@ -0,0 +1,13 @@
+//java program to print numberss from 10 to 1
+
+import java.io.*;
+
+public class TenToOne{
+
+  public static void main(String args[]){
+
+    for ( int i = 10 ; i >= 1 ; i--){
+      System.out.println(i);
+    }
+  }
+}
