323dc6a50fac008d8ac8b7d049a29921622e5c43
Sakshi Babar <sakshibabar17@gmail.com>
2026-04-02 21:25:34 +0530

    Create EvenOneToFifty.java to print even numbers
    Add Java program to print even numbers from 1 to 50.

---
+++ loops/EvenOneToFifty.java
@@ -0,0 +1,13 @@
+//java program to print even numbers from 1 to 50
+
+import java.io.*;
+
+public class EvenOneToFifty{
+
+  public static void main(String args[]) {
+
+    for (int i = 2 ; i <= 50 ; i += 2){
+      System.out.println(i);
+    }
+  }
+}
