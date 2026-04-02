b9e2a0d5730cfbc208808268c356edf3cc11d4f0
Sakshi Babar <sakshibabar17@gmail.com>
2026-04-02 21:17:14 +0530

    Add program to print numbers from 1 to 10

---
+++ loops/OneToTen.java
@@ -0,0 +1,14 @@
+//java program to print 1 to 10 numbers
+
+import java.util.Scanner;
+
+public class OneToTen{
+
+  public static void main(String args[]){
+
+    for (int i = 1 ; i <= 10 ; i++){
+      System.out.println(i+ " ");
+    }
+
+  }
+}
