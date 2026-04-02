275ab29c3dc15a314715b68d840ed37d8b443c92
Sakshi Babar <sakshibabar17@gmail.com>
2026-04-02 21:33:09 +0530

    Add Java program to print multiplication table

---
+++ loops/Table.java
@@ -0,0 +1,16 @@
+//java program to print multiplication table of a number
+
+import java.io.*;
+
+public class Table{
+
+  public static void main(String args[]) {
+
+    int n = 5;
+
+    for ( i = 1 ; i <= 10 ; i++ ) {
+
+      System.out.println(n + " * " + i + " = " + (n * i));
+    }
+  }
+}
