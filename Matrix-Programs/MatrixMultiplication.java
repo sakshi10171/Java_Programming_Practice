e17e1a32417de8c5bf88a691dcebf8a170864818
Sakshi Babar <sakshibabar17@gmail.com>
2026-07-02 20:34:22 +0530

    Add MatrixMultiplication.java for matrix operations

---
+++ Matrix-Programs/MatrixMultiplication.java
@@ -0,0 +1,66 @@
+//Java program to do multiplication of matrix
+
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows of first matrix: ");
+        int r1 = sc.nextInt();
+
+        System.out.print("Enter columns of first matrix: ");
+        int c1 = sc.nextInt();
+
+        System.out.print("Enter rows of second matrix: ");
+        int r2 = sc.nextInt();
+
+        System.out.print("Enter columns of second matrix: ");
+        int c2 = sc.nextInt();
+
+        if (c1 != r2) {
+            System.out.println("Matrix multiplication is not possible.");
+            return;
+        }
+
+        int[][] matrix1 = new int[r1][c1];
+        int[][] matrix2 = new int[r2][c2];
+        int[][] result = new int[r1][c2];
+
+        System.out.println("Enter first matrix:");
+
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter second matrix:");
+
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                for (int k = 0; k < c1; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        System.out.println("Multiplication of matrices:");
+
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
