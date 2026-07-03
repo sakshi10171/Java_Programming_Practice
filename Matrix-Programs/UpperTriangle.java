b0609624df38ffb58415cdee885db68f7d731fd8
Sakshi Babar <sakshibabar17@gmail.com>
2026-07-03 18:44:43 +0530

    Add UpperTriangle program to print upper triangular matrix
    This program prompts the user for a square matrix and prints its upper triangular elements, replacing lower triangular elements with zeros.

---
+++ Matrix-Programs/UpperTriangle.java
@@ -0,0 +1,39 @@
+//Java program to print upper triangular elements of matrix
+
+import java.util.Scanner;
+
+public class UpperTriangle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the square matrix: ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter the matrix elements:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Upper Triangular Matrix:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i <= j) {
+                    System.out.print(matrix[i][j] + " ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
